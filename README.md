ff86a2b28ad5c428d045d8df5ed42fc32833d13b
MR_CODEBOY <ahmed.01014502867.y@gmail.com>
2025-04-23 16:26:19 +0200

    Fix two

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + TypeScript + Vite
+# majorX
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const HOST = import.meta.env.VITE_SERVER_URL;
-
+// export const HOST = import.meta.env.VITE_SERVER_URL;
+export const HOST = "http://localhost:3030"
 const AUTH_ROUTES = "/api/auth";
 export const SIGNIN_ROUTE          = `${AUTH_ROUTES}/login`;
 export const SIGNUP_ROUTE          = `${AUTH_ROUTES}/register`;
